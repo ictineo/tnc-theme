e81055ad20d886b758d84aad817d9c3cce118e25
Juli Mestieri (Projecte Ictineo SCCL) <juli@projecteictineo.com>
2014-07-07 18:15:58 +0200

    condicionament del multimedia al camp estrenat

--- templates/node--fitxa-espectacle.tpl.php
+++ templates/node--fitxa-espectacle.tpl.php
@@ -1,5 +1,5 @@
 <?php
-//dsm($content);
+dsm($content);
 /**
  * @file
  * Returns the HTML for a node.
@@ -9,124 +9,130 @@
  */
 ?>
 <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <?php
-    $audio_elements = "";
-    /** Images field **/
-    foreach ($content['field_imatges']['#items'] as $i => $trash):
-      foreach ($content['field_imatges'][$i]['node'] as $nid => $img):
-        foreach ($img['field_imatge']['#items'] as $j => $elem):
-          $audio_elements .= '<figure class="image-figure">';
-          $audio_elements .= '  <img id="image-'.$i.'" src="'.file_create_url($elem['uri']).'" alt="'.$elem['description'].'" />';
-          $audio_elements .= '</figure>';
+  <div id="mm-node-region">
+  <?php if(isset($content['field_estrenat']['#items'][0]['value']) && $content['field_estrenat']['#items'][0]['value'] == '0'):?>
+    <?php print render($content['field_imatge_capcalera']); ?>
+  <?php else: ?>
+      <?php
+        $audio_elements = "";
+        /** Images field **/
+        foreach ($content['field_imatges']['#items'] as $i => $trash):
+          foreach ($content['field_imatges'][$i]['node'] as $nid => $img):
+            foreach ($img['field_imatge']['#items'] as $j => $elem):
+              $audio_elements .= '<figure class="image-figure">';
+              $audio_elements .= '  <img id="image-'.$i.'" src="'.file_create_url($elem['uri']).'" alt="'.$elem['description'].'" />';
+              $audio_elements .= '</figure>';
+            endforeach;
+          endforeach;
+        endforeach;
+        /** ja esta mostrat, no cal que torni a sortir **/
+        hide($content['feild_video']);
+        ?>
+      <div id="images-wrapper">
+        <div class="cycle-slideshow main"
+             data-cycle-fx="scrollHorz" 
+             data-cycle-timeout="0"
+             data-cycle-slides="> figure"
+            >
+          <?php print $audio_elements; ?>
+        </div>
+        <div class="cycle-slideshow pager"
+             data-cycle-fx="scrollHorz" 
+             data-cycle-timeout="0"
+             data-cycle-slides="> figure"
+             data-cycle-fx="carousel"
+             data-cycle-carousel-visible="5"
+             data-cycle-carousel-fluid=true
+            >
+          <?php print $audio_elements; ?>
+        </div>
+      </div>
+
+        <?php
+        $video_elements = "";
+        /** Video field **/
+        foreach ($content['field_videos']['#items'] as $i => $trash):
+          foreach ($content['field_videos'][$i]['node'] as $nid => $video):
+            foreach ($video['field_video']['#items'] as $j => $elem):
+              $video_elements .= '<figure class="video-figure">';
+              $video_elements .= '  <video id="audio-'.$i.'" preload="none">';
+              $video_elements .= '    <source type="'.$elem['filemime'].'" src="'.file_create_url($elem['uri']).'" />';
+              $video_elements .= '  </video>';
+              $video_elements .= '  <figcaption id="video-'.$i.'-description">';
+              $video_elements .= '    '.$elem['description'];
+              $video_elements .= '  </figcaption>';
+              $video_elements .= '</figure>';
+            endforeach;
+          endforeach;
+        endforeach;
+        /** ja esta mostrat, no cal que torni a sortir **/
+        hide($content['feild_video']);
+        ?>
+      <div id="video-wrapper">
+        <div class="cycle-slideshow"
+             data-cycle-fx="scrollHorz" 
+             data-cycle-timeout="0"
+             data-cycle-slides="> figure"
+            >
+          <?php print $video_elements; ?>
+        </div>
+        <div class="cycle-slideshow pager"
+             data-cycle-fx="scrollHorz" 
+             data-cycle-timeout="0"
+             data-cycle-slides="> figure"
+             data-cycle-fx="carousel"
+             data-cycle-carousel-visible="2"
+             data-cycle-carousel-fluid=true
+            >
+          <?php print $video_elements; ?>
+        </div>
+      </div>
+
+      <div id="audio-wrapper">
+      <?php
+      /** Audio field **/
+      foreach ($content['field_audio']['#items'] as $i => $trash):
+        foreach ($content['field_audio'][$i]['node'] as $nid => $audio):
+          foreach ($audio['field_file_audio']['#items'] as $j => $elem):
+            ?>
+            <figure class="audio-figure">
+              <audio id="audio-<?php print $i; ?>" preload="none">
+                <source type="<?php print $elem['filemime']; ?>" src="<?php print file_create_url($elem['uri']); ?>" />
+              </audio>
+              <figcaption id="audio-<?php print $i; ?>-description">
+                <?php print $elem['description']; ?>
+              </figcaption>
+            </figure>
+            <?php
+          endforeach;
         endforeach;
       endforeach;
-    endforeach;
-    /** ja esta mostrat, no cal que torni a sortir **/
-    hide($content['feild_video']);
-    ?>
-  <div id="images-wrapper">
-    <div class="cycle-slideshow main"
-         data-cycle-fx="scrollHorz" 
-         data-cycle-timeout="0"
-         data-cycle-slides="> figure"
-        >
-      <?php print $audio_elements; ?>
-    </div>
-    <div class="cycle-slideshow pager"
-         data-cycle-fx="scrollHorz" 
-         data-cycle-timeout="0"
-         data-cycle-slides="> figure"
-         data-cycle-fx="carousel"
-         data-cycle-carousel-visible="5"
-         data-cycle-carousel-fluid=true
-        >
-      <?php print $audio_elements; ?>
-    </div>
-  </div>
+      /** ja esta mostrat, no cal que torni a sortir **/
+      hide($content['feild_audio']);
+      ?>
+      </div>
 
-    <?php
-    $video_elements = "";
-    /** Video field **/
-    foreach ($content['field_videos']['#items'] as $i => $trash):
-      foreach ($content['field_videos'][$i]['node'] as $nid => $video):
-        foreach ($video['field_video']['#items'] as $j => $elem):
-          $video_elements .= '<figure class="video-figure">';
-          $video_elements .= '  <video id="audio-'.$i.'" preload="none">';
-          $video_elements .= '    <source type="'.$elem['filemime'].'" src="'.file_create_url($elem['uri']).'" />';
-          $video_elements .= '  </video>';
-          $video_elements .= '  <figcaption id="video-'.$i.'-description">';
-          $video_elements .= '    '.$elem['description'];
-          $video_elements .= '  </figcaption>';
-          $video_elements .= '</figure>';
+
+      <div id="document-wrapper">
+      <?php
+      /** Documents field **/
+      foreach ($content['field_documentacio']['#items'] as $i => $trash):
+        foreach ($content['field_documentacio'][$i]['node'] as $nid => $doc):
+          foreach ($doc['field_documents']['#items'] as $j => $elem):
+            ?>
+              <a href="<?php print file_create_url($elem['uri']); ?>">
+                <?php print $elem['description']; ?>
+              </a>
+            <?php
+          endforeach;
         endforeach;
       endforeach;
-    endforeach;
-    /** ja esta mostrat, no cal que torni a sortir **/
-    hide($content['feild_video']);
-    ?>
-  <div id="video-wrapper">
-    <div class="cycle-slideshow"
-         data-cycle-fx="scrollHorz" 
-         data-cycle-timeout="0"
-         data-cycle-slides="> figure"
-        >
-      <?php print $video_elements; ?>
-    </div>
-    <div class="cycle-slideshow pager"
-         data-cycle-fx="scrollHorz" 
-         data-cycle-timeout="0"
-         data-cycle-slides="> figure"
-         data-cycle-fx="carousel"
-         data-cycle-carousel-visible="2"
-         data-cycle-carousel-fluid=true
-        >
-      <?php print $video_elements; ?>
-    </div>
-  </div>
-
-  <div id="audio-wrapper">
-  <?php
-  /** Audio field **/
-  foreach ($content['field_audio']['#items'] as $i => $trash):
-    foreach ($content['field_audio'][$i]['node'] as $nid => $audio):
-      foreach ($audio['field_file_audio']['#items'] as $j => $elem):
-        ?>
-        <figure class="audio-figure">
-          <audio id="audio-<?php print $i; ?>" preload="none">
-            <source type="<?php print $elem['filemime']; ?>" src="<?php print file_create_url($elem['uri']); ?>" />
-          </audio>
-          <figcaption id="audio-<?php print $i; ?>-description">
-            <?php print $elem['description']; ?>
-          </figcaption>
-        </figure>
-        <?php
-      endforeach;
-    endforeach;
-  endforeach;
-  /** ja esta mostrat, no cal que torni a sortir **/
-  hide($content['feild_audio']);
-  ?>
-  </div>
-
-
-  <div id="document-wrapper">
-  <?php
-  /** Documents field **/
-  foreach ($content['field_documentacio']['#items'] as $i => $trash):
-    foreach ($content['field_documentacio'][$i]['node'] as $nid => $doc):
-      foreach ($doc['field_documents']['#items'] as $j => $elem):
-        ?>
-          <a href="<?php print file_create_url($elem['uri']); ?>">
-            <?php print $elem['description']; ?>
-          </a>
-        <?php
-      endforeach;
-    endforeach;
-  endforeach;
-  /** ja esta mostrat, no cal que torni a sortir **/
-  hide($content['feild_audio']);
-  ?>
-  </div>
+      /** ja esta mostrat, no cal que torni a sortir **/
+      hide($content['feild_audio']);
+      ?>
+      </div>
+    <?php endif; ?>
+  </div><!-- /mm-node-region -->
 
   <header>
     <div id="top-left4-wrapper">
