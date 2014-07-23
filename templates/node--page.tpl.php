5a58c656e273c2e80739012ce724f3eb92404acb
judejaco <judith@projecteictineo.com>
2014-07-23 18:55:11 +0200

    estils pagines estatiques

--- templates/node--page.tpl.php
+++ templates/node--page.tpl.php
@@ -38,8 +38,13 @@
   ?>
   <?php print render($content['field_imatge_capcalera']);?>
   <div class="tit_pagina"><h2><?php print render ($title);?></h2></div>
-  <?php print render($content);?>
-
+  <div class="entradeta"><?php print render ($content['field_resum_destacat']);?></div>
+  <div class="col_esq">
+    <div class="field-name-body"><?php print render ($content['body']);?></div>
+  </div>
+  <div class="col_dreta">
+    <div class="field-name-field-destacat-lateral"><?php print render ($content['field_destacat_lateral']);?></div>
+  </div>
   <?php print render($content['links']); ?>
 
   <?php print render($content['comments']); ?>
