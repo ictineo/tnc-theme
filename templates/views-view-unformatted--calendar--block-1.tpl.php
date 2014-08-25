31e0142c0ed6cb1d77b2150313b3ffd43dfed2b8
Juli Mestieri (Projecte Ictineo SCCL) <juli@projecteictineo.com>
2014-08-25 10:54:01 +0200

    netejat tpl calendar home

--- templates/views-view-unformatted--calendar--block-1.tpl.php
+++ templates/views-view-unformatted--calendar--block-1.tpl.php
@@ -52,6 +52,5 @@ foreach ($days as $date => $rendered) {
 ?>
 <?php $options = array('absolute' => TRUE);
       $url = url('calendari'); ?>
-?>    
   <span class="day cal_link"><a href="<?php print($url);?>">.</a></span>
 </div>
